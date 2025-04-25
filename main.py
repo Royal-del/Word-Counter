368c049e172457fc46b7b96efb42c831b605e495
Royal <roya.ehtesham191001@gmail.com>
2025-04-26 02:52:24 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+# Word Counter
+
+This script counts the number of words in a given sentence.
+
+## How to Use
+1. Run the program.
+2. Enter a sentence.
+3. The program will display the total number of words.
